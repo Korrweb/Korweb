c0f7cadc5669ba92186c2fbaae70dbdafcc96dc7
GuillemotClement <guillemot.clement@protonmail.com>
2025-01-24 23:56:49 +0100

    feat: add header and footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false,
     nesting: true,
-  })]
+  }), react()]
 });
